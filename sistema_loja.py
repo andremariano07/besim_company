6e366c1e57a0b7f42de0ea36de3658b71691eb60
andremariano07 <andrekratos@hotmail.com>
2026-02-09 18:35:06 -0300

    Update sistema_loja.py

--- sistema_loja.py
+++ sistema_loja.py
@@ -571,7 +571,7 @@ def force_attach_statusbar(win):
 DISABLE_AUTO_UPDATE = (
     False # <-- Evita que a atualização automática sobrescreva este patch
 )
-APP_VERSION = "5.3"
+APP_VERSION = "5.4"
 OWNER = "andremariano07"
 REPO = "besim_company"
 BRANCH = "main"
